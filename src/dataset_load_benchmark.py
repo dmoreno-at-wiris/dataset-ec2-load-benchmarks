c113c7f730b223eb49b728e07423764309bac985
Daniel <dmoreno@wiris.com>
2025-07-25 11:07:09 +0200

    feat(enrich-output): Add rich and make the benchmarks output more readable

--- src/dataset_load_benchmark.py
+++ src/dataset_load_benchmark.py
@@ -8,6 +8,8 @@ from enum import Enum
 
 from tqdm import tqdm
 from torch.utils.data import DataLoader
+# TODO: Import rich print
+from rich import print, markdown, table, align
 
 from src.timer import Timer
 
@@ -18,7 +20,7 @@ class DatasetSource(Enum):
     OTHER = -1
 
 
-class DatasetMode(Enum):
+class DatasetMode(Enum):        # TODO: Consider moving towards a more descriptive enums. E.g.: PARQUET, PARQUET_COMPRESSED, ...
     FILES = 0
     DF = 1
     COMPRESSED = 2
@@ -32,9 +34,10 @@ class DatasetLoadBenchmark:
     load_as: DatasetMode
     # dataset: Dataset
     dataloader: DataLoader
+    log_table: table.Table
+    benchmark_name: str = ""
     iterations_number: int = 4
     train_epochs: int = 1
-
     # train_features: ClassVar[Iterable]
     # train_labels: ClassVar[Iterable]
 
@@ -45,7 +48,7 @@ class DatasetLoadBenchmark:
         # print(f"First lable: {train_labels[0]}")
 
     def train_mock(self) -> float:
-        t = Timer(name=f"{self.load_from}-{self.load_as}-train-mock")
+        t = Timer(name=f"{self.load_from}-{self.load_as}-train-mock", text="**One epoch training time**:\n{:0.4f} seconds\n", logger=(lambda text : print(align.Align.center(markdown.Markdown(text)))))
         sum_t = 0
         for epoch in range(self.train_epochs):
             t.start()
@@ -62,11 +65,13 @@ class DatasetLoadBenchmark:
             sum_t += self.train_mock()
 
         mean_t = sum_t / self.iterations_number
-        print(
-            "\n\nMean time for getting all taining samples"
-            "\n---"
-            f"\n\t- loaded from {self.load_from}"
-            f"\n\t- as {self.load_as}"
-            f"\n\n Mean time: {mean_t}"
+
+        self.log_table.add_row(
+            self.benchmark_name,
+            f"{self.load_from}",
+            f"{self.load_as}",
+            f"{mean_t}",
         )
+        print(align.Align.center(self.log_table))
+        self.log_table = None
         return mean_t
